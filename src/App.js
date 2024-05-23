7e2c3143fee625951038cc45c1757c85be496bda
arockiyaraja <arockiyaraja88@gmail.com>
2024-05-23 23:05:29 +0530

    Tab Icon Added

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import InfinityScroll from './Components/InfinityScroll';
 import Projects from './Components/Projects';
 import Footer from "./Components/Footer";
 import HorizontalLine from "./Components/HorizontalLine"
+import ME from "./Images/tabicon.jpg"
 
 function App() {
 
   return (
     <div className=" w-[100%] h-screen overflow-y-scroll bg-gray-900" >
       <Helmet>
+        <link rel="icon" type="image/png" href={ME} className=' rounded-full' />
         <title>I'm Arockiya Raja — Full Stack Web Developer</title>
       </Helmet>
 
